4be43b37f492a42ae2328cc2ac3a1e6dbca6cc5a
Maxsuel Maccari <maxmaccari@hotmail.com>
2019-04-09 18:18:42 -0400

    Implement websockets
    https://www.youtube.com/watch?v=IoONU2WLfrA

--- src/server.js
+++ src/server.js
@@ -3,6 +3,14 @@ const mongoose = require('mongoose');
 const path = require('path');
 
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+io.on('connection', socket => {
+  socket.on('connectRoom', box => {
+    socket.join(box);
+  });
+});
 
 mongoose.connect(
   'mongodb+srv://omnistack:omnistack@mongoedu-ikipg.mongodb.net/omnistack?retryWrites=true',
@@ -11,10 +19,16 @@ mongoose.connect(
   }
 );
 
+app.use((req, res, next) => {
+  req.io = io;
+
+  return next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/files', express.static(path.resolve(__dirname, '..', 'tmp')));
 
 app.use(require('./routes'));
 
-app.listen(3333);
+server.listen(3333);
