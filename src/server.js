5340e3752b2fd0b011e268a5a05f20cfaf8a288c
Maxsuel Maccari <maxmaccari@hotmail.com>
2019-04-09 18:20:28 -0400

    Add cors
    https://www.youtube.com/watch?v=IoONU2WLfrA

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
+
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
